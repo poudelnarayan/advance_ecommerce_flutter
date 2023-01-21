124e9a8f83847b1834944f8f62f0500038debd2d
Narayan Poudel <poudelnarayan434@gmail.com>
2023-01-21 18:11:42 +0545

    starting with gorouter

--- lib/main.dart
+++ lib/main.dart
@@ -3,6 +3,7 @@ import 'dart:async';
 import 'package:ecommerce_app/src/app.dart';
 import 'package:ecommerce_app/src/localization/string_hardcoded.dart';
 import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
 
 void main() async {
   // * For more info on error handling, see:
@@ -10,6 +11,7 @@ void main() async {
   await runZonedGuarded(() async {
     WidgetsFlutterBinding.ensureInitialized();
     // * Entry point of the app
+    GoRouter.setUrlPathStrategy(UrlPathStrategy.path);
     runApp(const MyApp());
 
     // * This code will present some error UI if any uncaught exception happens
@@ -17,13 +19,14 @@ void main() async {
       FlutterError.presentError(details);
     };
     ErrorWidget.builder = (FlutterErrorDetails details) {
-      return Scaffold(
+      return MaterialApp(
+          home: Scaffold(
         appBar: AppBar(
           backgroundColor: Colors.red,
           title: Text('An error occurred'.hardcoded),
         ),
         body: Center(child: Text(details.toString())),
-      );
+      ));
     };
   }, (Object error, StackTrace stack) {
     // * Log any errors to console
