50a32edf29c4591b8eacaa548268282b9d0c4cc2
Narayan Poudel <poudelnarayan434@gmail.com>
2023-01-21 18:35:26 +0545

    go router pageBuilder

--- lib/src/routing/app_router.dart
+++ lib/src/routing/app_router.dart
@@ -1,4 +1,5 @@
 import 'package:ecommerce_app/src/features/shopping_cart/shopping_cart_screen.dart';
+import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
 
 import '../features/products_list/products_list_screen.dart';
@@ -13,7 +14,14 @@ final goRouter = GoRouter(
         routes: [
           GoRoute(
             path: 'cart',
-            builder: ((context, state) => const ShoppingCartScreen()),
+            // builder: ((context, state) => const ShoppingCartScreen()), => this produces a back button on the page
+            pageBuilder: ((context, state) => MaterialPage(
+                  // to make a full screen shopping cart
+                  key: state.pageKey,
+                  fullscreenDialog:
+                      true, // => this build the cross button on a page
+                  child: const ShoppingCartScreen(),
+                )),
           ),
         ]),
   ],
