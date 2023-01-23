3aef64dc0b0623be4267136021638f8eb60324bb
Narayan Poudel <poudelnarayan434@gmail.com>
2023-01-23 15:54:06 +0545

    The asyncValue.guar method

--- lib/src/features/authentication/presentation/account/account_screen_controller.dart
+++ lib/src/features/authentication/presentation/account/account_screen_controller.dart
@@ -8,15 +8,18 @@ class AccountScreenController extends StateNotifier<AsyncValue<void>> {
   final FakeAuthRepository authRepository;
 
   Future<bool> signOut() async {
-    try {
-      state = const AsyncValue<void>.loading();
-      await authRepository.signOut();
-      state = const AsyncValue<void>.data(null);
-      return true;
-    } catch (e, st) {
-      state = AsyncValue<void>.error(e, st);
-      return false;
-    }
+    // try {
+    //   state = const AsyncValue<void>.loading();
+    //   await authRepository.signOut();
+    //   state = const AsyncValue<void>.data(null);
+    //   return true;
+    // } catch (e, st) {
+    //   state = AsyncValue<void>.error(e, st);
+    //   return false;
+    // }
+    state = const AsyncValue<void>.loading();
+    state = await AsyncValue.guard(() => authRepository.signOut());
+    return state.hasError == false;
   }
 }
 
